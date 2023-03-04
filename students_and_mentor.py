6c9b102f6b1168d0e4bb7cf00019be9bcd3a0530
e12io898 <e12io898@ya.ru>
2023-03-05 01:26:11 +0300

    task_1

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -14,7 +14,9 @@ class Mentor:
         self.courses_attached = []
         
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) \
+                and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
